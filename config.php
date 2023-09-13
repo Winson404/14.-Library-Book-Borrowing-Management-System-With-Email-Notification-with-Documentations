49780e025305ce30efd618dc43b3cbe6688af301
Winson404 <sonerwin8@gmail.com>
2023-09-13 22:48:13 +0800

    config modified - db name

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php 
 	session_start();
-	$conn = mysqli_connect("localhost","root","","library_catalog.sql");
+	$conn = mysqli_connect("localhost","root","","library_catalog_enhanced");
 
 	date_default_timezone_set('Asia/Manila');
 ?>
