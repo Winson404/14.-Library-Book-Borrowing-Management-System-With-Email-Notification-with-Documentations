256abe1341c0dee7f0a27d2587e4f78a4e197381
Winson404 <sonerwin8@gmail.com>
2023-09-25 01:20:24 +0800

    finalized

--- librarycatalog/Admin/delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-$con = mysqli_connect("localhost","root","","library_catalog");
-$id=$_REQUEST['id'];
-$query = "DELETE FROM courses WHERE id=$id"; 
-$result = mysqli_query($con,$query) or die ( mysqli_error());
-header("Location: course.php"); 
-?>
